d58b4f1939ba7954d5cf7d6be383d25ebd84c205
Dflore35 <72370356+Dflore35@users.noreply.github.com>
2022-03-13 23:53:20 -0700

    Update togglehidden.js

--- togglehidden.js
+++ togglehidden.js
@@ -13,7 +13,7 @@ AFRAME.registerComponent("togglehidden", {
     let el = this.el;
 
     //get all elements in text frame
-    var els = sceneEl.querySelectorAll("#text");
+    var text = sceneEl.querySelector("#text");
 
     //get image frame
     var img = sceneEl.querySelector("#image");
@@ -22,9 +22,13 @@ AFRAME.registerComponent("togglehidden", {
 
     var b1 = sceneEl.querySelector("#startButton");
     var b2 = sceneEl.querySelector("#resetButton");
-    
+    var button = sceneEl.querySelector("#startButton");
+    var button2 = sceneEl.querySelector("#resetButton");
+   
     //get toggle button
     var tb = sceneEl.querySelector("#toggleButton");
+        var tb2 = sceneEl.querySelector("#toggleButton2");
+
     //get lable
     var label = sceneEl.querySelector("#earthLabel");
      
@@ -34,22 +38,21 @@ AFRAME.registerComponent("togglehidden", {
       {
        
         //change text and font size 
-        for (var i = 0; i < els.length; i++) 
-        {
-          els[i].setAttribute(
+        
+          text.setAttribute(
             "value",
             "The asteroid is most likely a survivor of multiple violent hit-and-run collisions, common when the solar system was forming.Thus Psyche may be able to tell us how Earth's core and the cores of other terrestrial planets came to be."
           );
-          let currSpacing = els[i].getAttribute("letter-spacing");
+          let currSpacing = text.getAttribute("letter-spacing");
 
-          let params = {
+          let params1 = {
             property: "letter-spacing",
             to: 5.0,
             dur: 10,
           };
-          els[i].setAttribute("animation", params);
-        }
-      
+          text.setAttribute("animation", params1);
+        
+      //make the earth go away
         
           	let params = {
 						property: 'opacity',
@@ -58,77 +61,76 @@ AFRAME.registerComponent("togglehidden", {
 					};
           img.setAttribute("animation", params);
         
-        //change the image back to psyche
-        //img.setAttribute("src", "");
         
         page = 1;
-        tb.setAttribute("value", "Next: Asteriod Collison");
+        tb.setAttribute("value", "Next");
         label.setAttribute("value","Psyche ->");
         label.setAttribute("color","White");
 
       } else  if (page==1)
       {
+
         //change the text and text size
-        for (var i = 0; i < els.length; i++) 
-        {
-          els[i].setAttribute(
+      
+          text.setAttribute(
             "value",
            "Make your own Asteriod collison");
-          let currOpacity = els[i].getAttribute("letter-spacing");
+          let currOpacity = text.getAttribute("letter-spacing");
+        let currpos = b1.getAttribute("position");
 
-          let params = {
+    
+          let params1 = {
             property: "letter-spacing",
             to: 7.0,
             dur: 10,
           };
-          els[i].setAttribute("animation", params);
-        }
+          text.setAttribute("animation", params1);
         
-
+       
+//show the buttons
         
           	let c = {
 						property: 'opacity',
 						to: 1.0,
 						dur: 50,
 					};
+      
+
           text1.setAttribute("animation", c);
           text2.setAttribute("animation", c);
           b1.setAttribute("animation", c);
           b2.setAttribute("animation", c);
+      
 
-
-               
-        //change the image back to earth
-        //img.setAttribute("src","Earth.png");
 
         page = 2;
-        tb.setAttribute("value", "Back: Why Psyche is important");
+        tb.setAttribute("value", "Back");
         label.setAttribute("value","");
         label.setAttribute("color","white");
+        
+         
         
 
       }
       else if(page ==2)
         {
           //change text and font size 
-        for (var i = 0; i < els.length; i++) 
-        {
-          els[i].setAttribute
+          text.setAttribute
           (
             "value","Deep within the terrestrial planets, including Earth, scientists infer the presence of metallic cores, but these lie unreachably far below the planets’ rocky mantles and crusts. The asteroid Psyche offers a unique window into these building blocks of planet formation and the opportunity to investigate a previously unexplored type of world."
 
           );
-          let currSpacing = els[i].getAttribute("letter-spacing");
+          let currSpacing = text.getAttribute("letter-spacing");
 
-          let params = {
+          let params1 = {
             property: "letter-spacing",
             to: 3.0,
             dur: 10,
           };
-          els[i].setAttribute("animation", params);
-        }
+          text.setAttribute("animation", params1);
+        
            
-
+//hide buttons and bring the Earth back 
          	let params = {
 						property: 'opacity',
 						to: 1.0,
@@ -146,25 +148,22 @@ AFRAME.registerComponent("togglehidden", {
           b2.setAttribute("animation", c);
           
         
-        
-        //change the image back to earth
-          //img.setAttribute("src", "Earth.png");
-        
+                
         page = 0;
-        tb.setAttribute("value", "Next: How might have Pysche formed?");
+        tb.setAttribute("value", "Next");
         label.setAttribute("value","Earth ->");
         label.setAttribute("color","white");
           
         }
     };
 
-    //Add EventListener
+    //EventListener
 
         this.el.addEventListener("click", this.toggleHide);
 
     
   },
-  //On Remove
+  //Remove
   
     remove: function () {
 
